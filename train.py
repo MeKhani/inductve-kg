5a3930f863f543a7cfb5883a92d2afdc22c29f6a
mekhai <mekh.ch@gmial.com>
2024-11-27 11:29:52 -0800

    add some file to read data from datasets

--- train.py
+++ train.py
@@ -1,6 +1,27 @@
+import os 
 from tqdm import tqdm
 import random
 # from model import InGram
 import numpy as np
 import torch
+from my_parser import parse
+from dataset import TrainData
+
+
+
+OMP_NUM_THREADS = 8
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
+torch.autograd.set_detect_anomaly(True)
+torch.backends.cudnn.benchmark = True
+torch.set_num_threads(8)
+torch.cuda.empty_cache()
+
+args = parse()
+print(type(args))
 print("run it ")
+assert args.data_name in os.listdir(args.data_path), f"{args.data_name} Not Found"
+path = args.data_path + args.data_name + "/"
+train = TrainData(path)
+
